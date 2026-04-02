241ff9b30a26e563701c5b7b082b20adb683f09d
Robson Perassoli <robsonperassoli@gmail.com>
2026-04-02 14:46:39 -0300

    setup better auth and drizzle

---
+++ api/src/db/index.ts
@@ -0,0 +1,12 @@
+import { Database } from "bun:sqlite";
+import { drizzle } from "drizzle-orm/bun-sqlite";
+import * as schema from "./schema";
+
+if (!process.env.DB_FILE_NAME) {
+  throw new Error("DB_FILE_NAME environment variable is not set");
+}
+
+export const db = drizzle({
+  client: new Database(process.env.DB_FILE_NAME),
+  schema,
+});
